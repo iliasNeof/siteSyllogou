ddb94e800848e55fd7b8713afe5ee4dbcd582981
iliasneof <i.a.neofotistos@gmail.com>
2026-08-30 21:19:55 +0300

    Fix mobiles3

--- wwwroot/Js/Index.js
+++ wwwroot/Js/Index.js
@@ -18,5 +18,18 @@ function copyToClipboard(elementId, button) {
     });
 }
 
+document.querySelectorAll('.navbar-nav .dropdown-toggle').forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (window.innerWidth < 992) {
+      e.stopImmediatePropagation();      // μπλοκάρει τον handler του template
+      var menu = el.nextElementSibling;
+      var open = menu.classList.toggle('show');
+      el.setAttribute('aria-expanded', open);
+    }
+  }, true);
+});
 
 
